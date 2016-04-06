4ce5643952b52c0499a5bcd2cbb3e79c97f5551d
Clifford Heath <clifford.heath@gmail.com>
2016-04-06 17:54:10 +1000

    Added new Rails Models generator

---
+++ lib/activefacts/compositions/traits/rails.rb
@@ -0,0 +1,176 @@
+require 'activefacts/compositions'
+require 'active_support'
+require 'digest/sha1'
+
+module ActiveFacts
+  module Metamodel
+    class Component
+      def rails
+        @rails_facet ||= ACTR::Component.new(self)
+      end
+    end
+
+    class Index
+      def rails
+        @rails_facet ||= ACTR::Index.new(self)
+      end
+    end
+
+    class ForeignKey
+      def rails
+        @rails_facet ||= ACTR::ForeignKey.new(self)
+      end
+    end
+
+    class Composite
+      def rails
+        @rails_facet ||= ACTR::Composite.new(self)
+      end
+    end
+  end
+
+  module Composition
+    module Traits
+      module Rails
+        def self.name_trunc name
+          if name.length > 63
+            hash = Digest::SHA1.hexdigest name
+            name = name[0, 53] + '__' + hash[0, 8]
+          end
+          name
+        end
+
+        def self.plural_name name
+          # Crunch spaces and pluralise the first part, all in snake_case
+          name.pop if name.is_a?(Array) and name.last == []
+          name = name[0]*'_' if name.is_a?(Array) and name.size == 1
+          if name.is_a?(Array)
+            name = ActiveSupport::Inflector.tableize((name[0]*'_').gsub(/\s+/, '_')) +
+              '_' +
+              ActiveSupport::Inflector.underscore((name[1..-1].flatten*'_').gsub(/\s+/, '_'))
+          else
+            ActiveSupport::Inflector.tableize(name.gsub(/\s+/, '_'))
+          end
+        end
+
+        def self.singular_name name
+          # Crunch spaces and convert to snake_case
+          name = name.flatten*'_' if name.is_a?(Array)
+          ActiveSupport::Inflector.underscore(name.gsub(/\s+/, '_'))
+        end
+
+        class Facet
+          def initialize base
+            @base = base
+          end
+
+          def method_missing m, *a, &b
+            @base.send(m, *a, &b)
+          end
+        end
+
+        class Component < Facet
+          def name
+            ACTR::singular_name(@base.name)
+          end
+
+          def type
+            type_name, params, constraints = *explode.type()
+            rails_type = case type_name
+              when /^Auto ?Counter$/i
+                'serial'        # REVISIT: Need to detect surrogate ID fields and handle them correctly
+
+              when /^[Ug]uid$/i
+                'uuid'
+
+              when /^Unsigned ?Integer$/i,
+                /^Integer$/i,
+                /^Signed ?Integer$/i,
+                /^Unsigned ?Small ?Integer$/i,
+                /^Signed ?Small ?Integer$/i,
+                /^Unsigned ?Tiny ?Integer$/i
+                length = nil
+                'integer'
+
+              when /^Decimal$/i
+                'decimal'
+
+              when /^Float$/i, /^Double$/i, /^Real$/i
+                'float'
+
+              when /^Fixed ?Length ?Text$/i, /^Char$/i
+                'string'
+              when /^Variable ?Length ?Text$/i, /^String$/i
+                'string'
+              when /^Large ?Length ?Text$/i, /^Text$/i
+                'text'
+
+              when /^Date ?And ?Time$/i, /^Date ?Time$/i
+                'datetime'
+              when /^Date$/i
+                'datetime'
+              when /^Time$/i
+                'time'
+              when /^Auto ?Time ?Stamp$/i
+                'timestamp'
+
+              when /^Money$/i
+                'decimal'
+              when /^Picture ?Raw ?Data$/i, /^Image$/i, /^Variable ?Length ?Raw ?Data$/i, /^Blob$/i
+                'binary'
+              when /^BIT$/i, /^Boolean$/i
+                'boolean'
+              else
+                type_name # raise "ActiveRecord type unknown for standard type #{type}"
+              end
+            [rails_type, params[:length]]
+          end
+        end
+
+        class Index < Facet
+          def name
+            column_names = columns.map{|c| c.rails.name }
+            index_name = "index_#{on.rails.name+'_on_'+column_names*'_'}"
+            ACTR.name_trunc index_name
+          end
+        end
+
+        class ForeignKey < Facet
+          # A foreign key is between two Composites, but involves some Absorption that traverses
+          # between two object types, either or both of which may be fully absorbed into the
+          # respective Composites. The name of a foreign key takes this into account.
+
+          def from_association_name
+            absorption.column_name.snakecase
+          end
+
+          def to_association
+            if absorption && absorption.child_role.is_unique
+              [ "has_one", source_composite.rails.singular_name]
+            else
+              [ "has_many", source_composite.rails.plural_name]
+            end
+          end
+        end
+
+        class Composite < Facet
+          def plural_name
+            ACTR::plural_name(@base.mapping.name)
+          end
+
+          def singular_name
+            ACTR::singular_name(@base.mapping.name)
+          end
+
+          def class_name
+            ActiveSupport::Inflector.camelize(@base.mapping.name.gsub(/\s+/, '_'))
+          end
+        end
+
+      end
+    end
+  end
+
+  private
+    ACTR = Composition::Traits::Rails
+end
---
+++ lib/activefacts/generator/rails/models.rb
@@ -0,0 +1,237 @@
+#
+#       ActiveFacts Rails Models Generator
+#       ActiveFacts Rails Schema Generator
+#
+# Copyright (c) 2009-2016 Clifford Heath. Read the LICENSE file.
+#
+require 'digest/sha1'
+require 'activefacts/metamodel'
+require 'activefacts/registry'
+require 'activefacts/compositions'
+require 'activefacts/generator'
+require 'activefacts/compositions/traits/rails'
+
+module ActiveFacts
+  module Generators
+    module Rails
+      class Models
+        HEADER = "# Auto-generated from CQL, edits will be lost"
+        def self.options
+          ({
+            output:     [String,    "Overwrite model files into this output directory"],
+            concern:    [String,    "Namespace for the concerns"],
+            validation: ['Boolean', "Disable generation of validations"],
+          })
+        end
+
+        def initialize composition, options = {}
+          @composition = composition
+          @options = options
+          @option_output = options.delete("output")
+          @option_concern = options.delete("concern")
+          @option_validations = options.include?('validations') ? options.delete("validations") : true
+        end
+
+        def warn *a
+          $stderr.puts *a
+        end
+
+        def generate
+          list_extant_files if @option_output
+
+          @ok = true
+          models =
+            @composition.
+            all_composite.
+            sort_by{|composite| composite.mapping.name}.
+            map{|composite| generate_composite composite}.
+            compact*"\n"
+
+          warn "\# #{@composition.name} generated with errors" unless @ok
+          delete_old_generated_files if @option_output
+
+          models
+        end
+
+        def list_extant_files
+          @preexisting_files = Dir[@option_output+'/*.rb']
+        end
+
+        def delete_old_generated_files
+          remaining = []
+          cleaned = 0
+          @preexisting_files.each do |pathname|
+            if generated_file_exists(pathname) == true
+              File.unlink(pathname) 
+              cleaned += 1
+            else
+              remaining << pathname
+            end
+          end
+          $stderr.puts "Cleaned up #{cleaned} old generated files" if @preexisting_files.size > 0
+          $stderr.puts "Remaining non-generated files:\n\t#{remaining*"\n\t"}" if remaining.size > 0
+        end
+
+        def generated_file_exists pathname
+          File.open(pathname, 'r') do |existing|
+            first_lines = existing.read(1024)     # Make it possible to pass over a magic charset comment
+            if first_lines.length == 0 or first_lines =~ %r{^#{HEADER}}
+              return true
+            end
+          end
+          return false    # File exists, but is not generated
+        rescue Errno::ENOENT
+          return nil      # File does not exist
+        end
+
+        def create_if_ok filename
+          # Create a file in the output directory, being careful not to overwrite carelessly
+          out = $stdout
+          if @option_output
+            pathname = (@option_output+'/'+filename).gsub(%r{//+}, '/')
+            @preexisting_files.reject!{|f| f == pathname }    # Don't clean up this file
+            if generated_file_exists(pathname) == false
+              warn "not overwriting non-generated file #{pathname}"
+              @ok = false
+              return nil
+            end
+            out = File.open(pathname, 'w')
+          end
+          out
+        end
+
+        def generate_composite composite
+          model =
+            (@option_concern ? "module #{@option_concern}\n" : '') +
+            model_body(composite).gsub(/^./, @option_concern ? '  \0' : '\0') +
+            (@option_concern ? "end\n" : '')
+
+          return model unless @option_output
+
+          filename = composite.rails.singular_name+'.rb'
+          out = create_if_ok(filename)
+          return nil unless out
+          out.puts "#{HEADER}\n\n"+model
+        ensure
+          out.close if out
+          nil
+        end
+
+        def model_header composite
+          [
+          "module #{composite.rails.class_name}",
+          "  extend ActiveSupport::Concern",
+          "  included do"
+          ]
+        end
+
+        def model_key composite
+          identifier_columns = composite.primary_index.all_index_field
+          if identifier_columns.size == 1
+            [
+            "    self.primary_key = '#{identifier_columns.single.component.column_name.snakecase}'",
+            ''    # Leave a blank line
+            ]
+          else
+            []
+          end
+        end
+
+        def model_body composite
+          (
+            model_header(composite) +
+            model_key(composite) +
+            to_associations(composite) +
+            from_associations(composite) +
+            column_constraints(composite) +
+            [
+            "  end",
+            "end"
+            ]
+          ).
+          compact.
+          map{|l| l+"\n"}.
+          join('').
+          gsub(/\n\n\n+/,"\n\n")  # At most double-spaced
+        end
+
+        def to_associations composite
+          # Each outbound foreign key generates a belongs_to association:
+          composite.all_foreign_key_as_source_composite.
+          sort_by{ |fk| fk.all_foreign_key_field.map(&:component).flat_map(&:path).map(&:rank_key) }.
+          flat_map do |fk|
+            association_name = fk.rails.from_association_name
+
+            if association_name != fk.composite.rails.singular_name
+              # A different class_name is implied, emit an explicit one:
+              class_name = ", :class_name => '#{fk.composite.rails.class_name}'"
+            end
+
+            foreign_key = ", :foreign_key => :#{fk.all_foreign_key_field.single.component.column_name.snakecase}"
+            if foreign_key == fk.composite.rails.singular_name+'_id'
+              # See lib/active_record/reflection.rb, method #derive_foreign_key
+              foreign_key = ''
+            end
+
+            [
+            fk.absorption ? "    \# #{fk.absorption.comment}" : nil,
+            "    belongs_to :#{association_name}#{class_name}#{foreign_key}",
+            fk.absorption ? '' : nil,
+            ]
+          end.compact
+        end
+
+        def from_associations composite
+          # has_one/has_many Associations
+          composite.all_foreign_key_as_target_composite.
+          sort_by{ |fk| fk.all_foreign_key_field.map(&:component).flat_map(&:path).map(&:rank_key) }.
+          flat_map do |fk|
+
+            if fk.all_foreign_key_field.size > 1
+              raise "Can't emit Rails associations for multi-part foreign key with #{fk.references.inspect}. Did you mean to use --surrogate?"
+            end
+
+            association_type, association_name = *fk.rails.to_association
+
+            [
+              # REVISIT: We want the reverse-order comment here really
+              fk.absorption ? "    \# #{fk.absorption.comment}" : nil,
+              %Q{    #{association_type} :#{association_name}} +
+              %Q{, :class_name => '#{fk.source_composite.rails.class_name}'} +
+              %Q{, :foreign_key => :#{fk.all_foreign_key_field.single.component.column_name.snakecase}} +
+              %Q{, :dependent => :destroy}
+            ] +
+            # If fk.absorption.source_composite is a join table, we can emit a has_many :through for each other key
+            # REVISIT: We could alternately do this for all belongs_to's in the source composite
+            if fk.source_composite.primary_index.all_index_field.size > 1
+              fk.source_composite.primary_index.all_index_field.map(&:component).flat_map do |ic|
+                next nil if ic.is_a?(MM::Indicator)      # or use rails.plural_name(ic.references[0].to_names) ?
+                onward_fks = ic.all_foreign_key_field.map(&:foreign_key)
+                next nil if onward_fks.size == 0 or onward_fks.detect{|fk| fk.composite == composite} # Skip the back-reference
+                # REVISIT: This far association name needs to be augmented for its role name
+                "    has_many :#{onward_fks[0].composite.rails.plural_name}, :through => :#{association_name}"
+              end.compact
+            else
+              []
+            end
+          end.compact
+        end
+
+        def column_constraints composite
+          return [] unless @option_validations
+          ccs =
+            composite.mapping.all_leaf.flat_map do |component|
+              next unless component.path_mandatory
+              next if component.is_a?(Metamodel::Mapping) && component.object_type.is_a?(Metamodel::ValueType) && component.is_auto_assigned
+              [ "    validates :#{component.column_name.snakecase}, :presence => true" ]
+            end.compact
+          ccs.unshift("") unless ccs.empty?
+          ccs
+        end
+
+      end
+      MM = ActiveFacts::Metamodel
+    end
+    publish_generator Rails::Models
+  end
+end
